c020389fbc62619020adad8ca4ede1909512af17
Tom <T_S_Graphics@hotmail.co.uk>
2018-10-05 18:47:07 +0100

    Initial Commit - Base Repo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CalendarViewComponent } from './shared/calendar-view/calendar-view.component';
+import { CalendarDetailComponent } from './shared/calendar-detail/calendar-detail.component';
+import { SplashScreenComponent } from './display/splash-screen/splash-screen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendarViewComponent,
+    CalendarDetailComponent,
+    SplashScreenComponent
   ],
   imports: [
     BrowserModule
